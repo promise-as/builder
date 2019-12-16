a70c1e56ecfe1f3ca4aad48ea240b9329bd80b85
Hong Baojin <13809735212@163.com>
2019-12-16 18:45:06 +0800

    first commit

---
+++ dist/js/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+$(function () {
+  theaMsForm($('.sign-in'));
+
+  /**
+   * @param { 目标元素 } aimEle 
+   * @param { 内容元素 } contEle 
+   * @param { 类名 } className 
+   */
+  function tabHandle(aimEle, contEle, className) {
+    $(aimEle).each(function (i) {
+      $(this).mouseenter(function () {
+        $(this).addClass(className).siblings().removeClass(className);
+        $($(contEle)[i]).addClass(className).siblings().removeClass(className);
+      });
+    });
+  }
+  tabHandle('.builder-nav li', '.builder-inquire .inquire-item', 'cur');
+
+  /**
+   * @param { 点击元素 } clickEle 
+   * @param { 响应元素 } responseEle 
+   * @param { 类名 } className 
+   * @param { 标识 } flag  true：选中，false：未选中
+   */
+  function radioHandle(clickEle, responseEle, className, flag) {
+    $(clickEle).click(function () {
+      $(this).toggleClass(className);
+      if (flag) {
+        $(responseEle).addClass('disable').attr('disabled', 'disabled');
+      } else {
+        $(responseEle).removeClass('disable').removeAttr("disabled");
+      }
+      flag = !flag;
+    });
+  }
+  radioHandle('#radioBtn', '#submit', 'sel', 'true');
+});
