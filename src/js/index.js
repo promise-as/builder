d166e2e15adc3718efb7de6b65bfa46d01ec32c6
Hong Baojin <13809735212@163.com>
2019-12-17 17:54:56 +0800

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,30 @@
 $(function () {
-  theaMsForm($('.sign-in'));
+  theaMsForm($('.sign-in'), function () {
+    // 显示弹窗
+    $("#popup").show();
+    // 关闭弹窗
+    $("#closePopup").click(function () {
+      $("#popup").hide();
+    });
+  });
 
   /**
    * @param { 目标元素 } aimEle 
    * @param { 内容元素 } contEle 
    * @param { 类名 } className 
    */
-  function tabHandle(aimEle, contEle, className){
-    $(aimEle).each(function(i){
-      $(this).mouseenter(function(){
+  function tabHandle(aimEle, contEle, className) {
+    $(aimEle).each(function (i) {
+      $(this).click(function () {
         $(this).addClass(className).siblings().removeClass(className);
         $($(contEle)[i]).addClass(className).siblings().removeClass(className);
-      })
-    })
+      });
+    });
   }
   tabHandle('.builder-nav li', '.builder-inquire .inquire-item', 'cur');
 
   /**
+   * 表单的查询协议
    * @param { 点击元素 } clickEle 
    * @param { 响应元素 } responseEle 
    * @param { 类名 } className 
@@ -24,14 +32,14 @@ $(function () {
    */
   function radioHandle(clickEle, responseEle, className, flag) {
     $(clickEle).click(function () {
-      $(this).toggleClass(className)
+      $(this).toggleClass(className);
       if (flag) {
         $(responseEle).addClass('disable').attr('disabled', 'disabled');
       } else {
         $(responseEle).removeClass('disable').removeAttr("disabled");
       }
       flag = !flag;
-    })
+    });
   }
-  radioHandle('#radioBtn', '#submit', 'sel', 'true')
-})
+  // radioHandle('#radioBtn', '#submit', 'sel', 'true');
+});
